5e5e3350514647dc1f3487bab47830dafc6fffff
cyber_monkey <7063275+miguelcarrillo@users.noreply.github.com>
2021-10-27 10:40:00 -0500

    Home page, Profile Page, Post Page with real time data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,63 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Link from 'next/link'
-
 import Loader from '../components/Loader';
-import toast from 'react-hot-toast';
+import PostFeed from '../components/PostFeed';
+import { firestore, fromMillis, postToJSON } from '../lib/firebase';
+import { useState } from 'react';
 
-export default function Home() {
+const LIMIT = 1;
+
+export async function getServerSideProps(context) {
+  const postsQuery = firestore
+  .collectionGroup('posts')
+  .where('published', '==', true)
+  .orderBy('createdAt', 'desc')
+  .limit(LIMIT);
+
+  const posts = (await postsQuery.get()).docs.map(postToJSON);
+
+  return {
+    props: { posts },
+  };
+}
+
+export default function Home(props) {
+  const [posts, setPosts] = useState(props.posts);
+  const [loading, setLoading] = useState(false);
+  const [postsEnd, setPostsEnd] = useState(false);
+
+  const getMorePosts = async () => {
+    setLoading(true);
+    const last = posts[posts.length - 1];
+
+    const cursor = typeof last.createdAt === 'number' ? fromMillis(last.createdAt) : last.createdAt;
+
+    const query = firestore
+    .collectionGroup('posts')
+    .where('published', '==', true)
+    .orderBy('createAt', 'desc')
+    .startAfter(cursor)
+    .limit(LIMIT);
+
+    const newPosts = (await query.get()).docs.map((doc) => doc.data());
+
+    setPosts(posts.concat(newPosts));
+    setLoading(false);
+
+    if(newPosts.length < LIMIT) {
+      setPostsEnd(true);
+    }
+  }
+
   return (
-    <div>
-      <button onClick={() => toast.success('hello toast!')}>
-        Toast me.
-      </button>
-    </div>
+    <main>
+      <PostFeed posts={posts} admin={false} />
+
+      {!loading && !postsEnd && <button onClick={getMorePosts}>Load More</button>}
+
+      <Loader show={loading} />
+
+      {postsEnd && 'You have reached the end!'}
+
+    </main>
   )
 }
